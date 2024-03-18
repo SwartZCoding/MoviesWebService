5c2a67ec9a6c01cbd3ec105f2c7c95058b9e6774
SwartZCoding <seaflyscontact@gmail.com>
2024-03-18 09:19:59 +0100

    add tests

--- tests/functional/users/integration.spec.ts
+++ tests/functional/users/integration.spec.ts
@@ -2,11 +2,17 @@ import {test} from '@japa/runner'
 import User from "App/Models/User";
 import Role from "App/Enums/Role";
 
-test('Create user', async () => {
-  const user = new User()
-  user.email = 'email@email.com'
-  user.password = 'secret'
-  user.role = Role.USER
-  await user.save()
+test.group('Create user & create movie', () => {
+  test('create user', async () => {
+    const user = new User()
+    user.email = 'email@email.com'
+    user.password = 'secret'
+    user.role = Role.ADMIN
+    await user.save()
+  })
 
+  test('create a movie', async ({ client }) => {
+    const response = await client.post('/movie/NomTest')
+    console.log(response.body())
+  })
 })
