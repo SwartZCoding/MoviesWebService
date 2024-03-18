59b858777842d0340141e6d18d192b2cb902192c
SwartZCoding <seaflyscontact@gmail.com>
2024-03-18 09:07:04 +0100

    try to fix test

--- tests/functional/categories/integration.spec.ts
+++ tests/functional/categories/integration.spec.ts
@@ -1,14 +1,15 @@
 import { test } from '@japa/runner'
 
 test.group('MoviesController Integration Tests', () => {
-  test('creates a movie with valid data', async ({ assert , client }) => {
-    const response = await client.post('/movies', {"name": "chibre" })
+  test('creates a movie with valid data', async ({assert, client}) => {
+    const response = await client.post('/movies')
     assert.equal(response.status(), 200)
     console.log(response.body())
   })
 
-  test('retrieves a movie by id', async ({ assert }, client ) => {
+  test('retrieves a movie by id', async ({assert, client}) => {
     const response = await client.get('/movies/{id}')
     assert.equal(response.status(), 200)
     // Assertions supplémentaires
   })
+})
